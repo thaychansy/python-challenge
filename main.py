56984f3fe5f041301dbb8247fefb892cfed31092
thaychansy <thay.chansy@gmail.com>
2024-05-14 20:29:22 -0700

    Updates to main.py file

--- main.py
+++ main.py
@@ -4,23 +4,21 @@ import csv
 # Initialize variables
 total_months = 0
 total_profit_loss = 0
-previous_profit_loss = None  # To track changes
+previous_profit_loss = None  # Track changes
 max_increase = float('-inf')  
 min_decrease = float('inf')  
 max_increase_date = None
 min_decrease_date = None
-monthly_change = []
-monthly_difference = []
 previous_profit_loss = 0
 changes = []
 dates = []
 
 
 # Define the relative directory path
-relative_dir = '..\python-challenge\PyBank\Resources'
+relative_input_dir = '..\python-challenge\PyBank\Resources'
 
 # Join the current directory with the relative directory
-budget_csv = os.path.join(relative_dir,'budget_data.csv')
+budget_csv = os.path.join(relative_input_dir,'budget_data.csv')
 
 # Open the CSV file in read mode
 with open(budget_csv, 'r') as csvfile:
@@ -58,15 +56,26 @@ min_decrease_date = dates[changes.index(min_decrease)]
 
 # Print results
 print(f"Financial Analysis")
-print(f"----------------------------------------------------")
+print(f"---------------------------------------------------------------")
 print(f"Total Months: {total_months}")
 print(f"Total: ${total_profit_loss:,.2f}")
-#print(f"Average Change: ${average_change:,.2f}")
+print(f"Average Change: ${average_change:,.2f}")
 print(f"Greatest Increase in Profits: {max_increase_date} (${max_increase:,.2f})")
 print(f"Greatest Decrease in Profits: {min_decrease_date} (${min_decrease:,.2f})")
 
 # Set variables for output file
+# Define the relative directory path
+relative_output_dir = '..\python-challenge\PyBank\Analysis'
 
+# Join the current directory with the relative directory
+analysis_output = os.path.join(relative_output_dir, "PyBank Financial Analysis")
 
-
-
+# Write the results in the PyBannk Financial Analysis.txt file
+with open(analysis_output, "w") as output_csv_file:
+    output_csv_file.write("Financial Analysis\n")
+    output_csv_file.write("---------------------------------------------------------------\n")
+    output_csv_file.write(f"Total Months: {total_months}\n")
+    output_csv_file.write(f"Total: ${int(total_profit_loss)}\n")
+    output_csv_file.write(f"Average Change: ${round(average_change,2)}\n")
+    output_csv_file.write(f"Greatest Increase in Profits: {max_increase_date} (${max_increase})\n")
+    output_csv_file.write(f"Greatest Decrease in Profits: {min_decrease_date} (${min_decrease})\n")
